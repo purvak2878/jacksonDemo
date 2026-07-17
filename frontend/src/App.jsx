ae56d58ab445e5cbec52395b6afb5bfd086b3411
purvak2878 <purvakulkarni456@gmail.com>
2026-07-17 22:01:10 +0530

    Added the first React Example

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="container">
+            <h1>Welcome</h1>
+
+            <h2>My First React Application</h2>
+
+            <p>Hello! My name is Purva Kulkarni.</p>
+
+            <p>
+                I am learning React and Spring Boot to become a Full Stack Developer.
+            </p>
+
+            <button>Thank You</button>
+        </div>
+    );
+}
+
+export default App;
